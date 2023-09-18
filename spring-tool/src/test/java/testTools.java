0caab1618ad3c3c80f02a53f8a5d37e8527d6a88
LTisme <284618025@qq.com>
2023-09-18 15:27:26 +0800

    已了解Spring的表达式————SpEL的简单使用

--- spring-tool/src/test/java/testTools.java
+++ spring-tool/src/test/java/testTools.java
@@ -6,8 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @Date: 2023/9/14
@@ -66,5 +70,30 @@ public class testTools {
         // 获得校验结果
         BindingResult bindingResult = dataBinder.getBindingResult();
         logger.info("{}", bindingResult);
+    }
+
+    // 测试Spring表达式，可能现在看着功能很鸡肋，但是它可以用在某些场景里
+    @Test
+    public void testSpEl(){
+        int order = 1;
+        ConcurrentHashMap<Integer, String> hashMap = new ConcurrentHashMap<>();
+        // 构造一个解析器
+        SpelExpressionParser parser = new SpelExpressionParser();
+        // 解析
+        Expression exp = parser.parseExpression("'Hello SpEl'");
+        // 获取解析的结果
+        String message = (String) exp.getValue();
+        hashMap.put(order++, message);
+        hashMap.put(order++, (String) parser.parseExpression("'Hello SpEl'.concat('!')").getValue());
+        hashMap.put(order++, (String) parser.parseExpression("new String('Hello SpEl').toUpperCase()").getValue());
+        // SpEL更常见的用法是提供一个针对特定对象实例（称为根对象）求值的表达式字符串。区别是在getValue的時候要传入对象
+        Employee employee = new Employee();
+        employee.setName("lucy");
+        hashMap.put(order++, (String) parser.parseExpression("name").getValue(employee));
+
+
+        hashMap.forEach((key, val) -> {
+            System.out.println(key + " -> " + val);
+        });
     }
 }
